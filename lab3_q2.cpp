0cf435c4e13c95f21bd266d3fba36a2ff1f59a04
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2018-08-16 13:18:05 +0000

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,35 @@
+//First include library
+#include<iostream>
+using namespace std;
+//Declare the main function
+int main()
+{
+//declaring two integer variables
+int a, b;
+//initializing values to the variables
+a=3;
+b=9;
+//finding the sum of a and b
+int sum=a+b;
+//displaying the sum of a and b
+cout << "Sum of a and b =" <<sum <<endl;
+//finding the difference of a and b
+int diff=a-b;
+//displaying the difference of a and b
+cout << "Difference of a and b =" <<diff <<endl;
+//finding the multiplication of a and b
+int mul=a*b;
+//displaying the multiplication of a and b
+cout << "Multiplication of a and b =" <<mul <<endl;
+//dividing a and b
+int div=b/a;
+//displaying division of a and b
+cout << "Dividing b by a =" <<div <<endl;
+//finding modulus of a and b
+int m=b%a;
+//displaying modulus of a and b
+cout << "The modulus of b over a =" <<m <<endl;
+//return any integer value to int function
+return 0;
+}
+ 
